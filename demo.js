1460664616960c7c22ca76f75ff2f087c613d658
Bipin Singh <bipins867@gmail.com>
2023-07-23 00:10:21 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,1 +1,10 @@
-console.log("MY first Node Js Project on Git");
+
+var lst=document.getElementsByTagName('li');
+
+
+lst[2].style.backgroundColor='green'
+
+for(i=0;i<lst.length;i++){
+    lst[i].style.fontWeight='bold'
+}
+
