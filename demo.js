5b8032f34e6b9835200a33d3ca6de740deac288b
Bipin Singh <bipins867@gmail.com>
2023-07-23 15:16:35 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,10 +1,16 @@
 
-var lst=document.getElementsByTagName('li');
+// var lst=document.getElementsByTagName('li');
 
 
-lst[2].style.backgroundColor='green'
+// lst[2].style.display="None"
 
-for(i=0;i<lst.length;i++){
-    lst[i].style.fontWeight='bold'
+// lst[1].style.backgroundColor='green'
+
+
+var li=document.querySelectorAll('li')
+
+li[1].style.color='red'
+
+for(i=1;i<li.length;i=i+2){
+    li[i].style.backgroundColor='green'
 }
-
